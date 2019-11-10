216b2eb1e80fe721aa46f0fd3bb1878e07c0cd84
Nick <yangyongdun@126.com>
2019-11-10 15:22:53 +1300

    change Form.Item -> Item

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,6 +4,8 @@ import { Form, Icon, Input, Button} from 'antd';
 import './login.less'
 import logo from './images/logo.png'
 
+const Item = Form.Item;
+
 /*
 Login Component
 */
@@ -23,24 +25,24 @@ export default class Login extends Component{
                 <section className="login-content">
                     <h2>User Login</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Form.Item>
+                        <Item>
                             <Input
                                 prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                 placeholder="Username"
                             />
-                        </Form.Item>
-                        <Form.Item>
+                        </Item>
+                        <Item>
                             <Input
                                 prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                 type="password"
                                 placeholder="Password"
                             />
-                        </Form.Item>
-                        <Form.Item>
+                        </Item>
+                        <Item>
                             <Button type="primary" htmlType="submit" className="login-form-button">
                                 Log in
                             </Button>
-                        </Form.Item>
+                        </Item>
                     </Form>
                 </section>
             </div>
