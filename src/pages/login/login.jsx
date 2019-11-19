5985cda3cb1a36e490a18696ea03e062d3bc7dd7
Nick <yangyongdun@126.com>
2019-11-19 22:30:50 +1300

    add username validate for login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -9,13 +9,25 @@ const Item = Form.Item;
 /*
 Login Component
 */
-export default class Login extends Component{
+class Login extends Component{
 
-    handleSubmit = () => {
+    handleSubmit = (event) => {
+        // Prevent Form auto submit
+        event.preventDefault();
+
+        const form = this.props.form;
         
+        // Get input data from form
+        const values = form.getFieldsValue(); 
+        console.log(values);
     }
 
     render() {
+        
+        // Get the form which pass from WrapLogin component
+        const form = this.props.form;
+        const {getFieldDecorator} = form;
+
         return (
             <div className="login">
                 <header className="login-header">
@@ -26,17 +38,32 @@ export default class Login extends Component{
                     <h2>User Login</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Item>
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />
+                            {
+                                getFieldDecorator('username', {
+                                    rules: [
+                                        { required: true, message: 'Please input your username!' },
+                                        { min: 4, message: 'The min length of Username is 4 !' },
+                                        { max: 12, message: 'The max lenght of Username is 12!' },
+                                        { pattern: /^[a-zA-Z0-9_]+$/, message: "Username should 'alpha', 'number' or '_' !" },
+                                    ],
+                                })(
+                                    <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                    />,
+                                )
+                            }
                         </Item>
                         <Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />
+                            {
+                                getFieldDecorator('password', {})(
+                                    <Input
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        type="password"
+                                        placeholder="Password"
+                                    />
+                                )
+                            }
                         </Item>
                         <Item>
                             <Button type="primary" htmlType="submit" className="login-form-button">
@@ -49,3 +76,15 @@ export default class Login extends Component{
         )
     }
 }
+
+/* 
+包装Form组件生成一个新的组件：Form(Login)
+新组件会向Form组件提供一个强大的对象属性：form
+*/
+const WrapLogin = Form.create()(Login);
+export default WrapLogin;
+
+/*
+1.前台表单验证
+2.收集表单输入数据
+*/
